205a8531253db55c04b94e9ebee44ace0a67d2fe
user131656 <user131656@ny218.local>
2017-12-11 17:39:15 -0500

    setting up view as SCNView

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -3,6 +3,10 @@ import SceneKit
 
 class GameViewController: UIViewController {
     
+    // property that renders the cnotent of the SCNView on the display
+    var scnView: SCNView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -14,4 +18,10 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    func setupView() {
+        // casting self.view to SCNView and store it in the scnView property
+        scnView = self.view as! SCNView
+    }
+    
 }
